0deef451a24f47c3e8c2bb171c51b2121491c055
jimny1016 <jimny1014@gmail.com>
2023-08-11 20:28:57 +0800

    Add menuData.

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -8,8 +8,8 @@ namespace WebApplication2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,8 +18,14 @@ namespace WebApplication2.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public HellowoldClass Get()
+        {
+            return new HellowoldClass() { Hey = "HelloWorld" };
+        }
+
+        [HttpGet("GetWeatherForecast2")]
+        public HellowoldClass Get2(string ddd)
         {
             return new HellowoldClass() { Hey = "HelloWorld" };
         }
